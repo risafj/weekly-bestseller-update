5503f41519502018f33b382de2eec34aab5f93b7
Risa Fujii <risa@kray.jp>
2020-01-26 15:15:55 +0900

    Put getNytList in a different function

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const axios = require('axios')
-const nytBookRootUrl = 'https://api.nytimes.com/svc/books/v3/lists/current'
-const fictionPath = '/combined-print-and-e-book-fiction.json'
-// const nonFictionPath = '/combined-print-and-e-book-nonfiction.json'
-const params = {'api-key': process.env.NYT_API_KEY }
 
 exports.handler = async (event) => {
-  const list = await axios.get(
-    nytBookRootUrl + fictionPath,
-    { params: params }
-  ).catch((error) => {
+  const nytList = await getNytList().catch((error) => {
     console.log(`Error: ${error}`)
   })
 
-  if (list) {
-    console.log(list)
+  if (nytList) {
+    console.log(nytList)
   }
+}
+
+async function getNytList() {
+  const nytBookRootUrl = 'https://api.nytimes.com/svc/books/v3/lists/current'
+  const fictionPath = '/combined-print-and-e-book-fiction.json'
+  // const nonFictionPath = '/combined-print-and-e-book-nonfiction.json'
+  const params = {'api-key': process.env.NYT_API_KEY }
+
+  return axios.get(
+    nytBookRootUrl + fictionPath,
+    { params: params }
+  )
 }
