abbe5cdf77d584c7b86b02c6084b5b503262e1c9
Risa Fujii <risa@kray.jp>
2020-01-26 15:10:37 +0900

    Use await/catch instead of then/catch

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const axios = require('axios')
+const nytBookRootUrl = 'https://api.nytimes.com/svc/books/v3/lists/current'
+const fictionPath = '/combined-print-and-e-book-fiction.json'
+// const nonFictionPath = '/combined-print-and-e-book-nonfiction.json'
+const params = {'api-key': process.env.NYT_API_KEY }
 
 exports.handler = async (event) => {
-  await getNYTList()
-}
-
-async function getNYTList () {
-  const nytBookRootUrl = 'https://api.nytimes.com/svc/books/v3/lists/current'
-  const fictionPath = '/combined-print-and-e-book-fiction.json'
-  // const nonFictionPath = '/combined-print-and-e-book-nonfiction.json'
-  const params = {'api-key': process.env.NYT_API_KEY }
-
-  await axios.get(
+  const list = await axios.get(
     nytBookRootUrl + fictionPath,
     { params: params }
-  )
-  .then(function (response) {
-    console.log(response.data.results.books)
+  ).catch((error) => {
+    console.log(`Error: ${error}`)
   })
-  .catch(function (error) {
-    // Add error handling at some point
-    console.log(error)
-  })
+
+  if (list) {
+    console.log(list)
+  }
 }
