1d555bf0e00ac29e2e1312969e115079d2858f2f
Risa Fujii <risa@kray.jp>
2020-02-04 09:32:40 +0900

    WIP

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const axios = require('axios')
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk')
+AWS.config.update({
+  region: 'ap-northeast-1'
+})
 const dynamo = new AWS.DynamoDB.DocumentClient()
 
 exports.handler = async (event) => {
-  const nytList = await getNytList().catch((error) => {
-    console.log(`Error: ${error}`)
-  })
+  // const nytList = await getNytList().catch((error) => {
+  //   console.log(`Error: ${error}`)
+  //   return
+  // })
 
-  if (nytList) {
-    console.log(nytList)
+  const params = {
+    TableName: 'weekly-bestseller-updates'
   }
+  const result = await dynamo.scan(params).promise().catch((error) => {
+    console.log(`error: ${error}`)
+    return
+  })
+  console.log(`result: ${result.Items}`)
 }
 
 async function getNytList() {
