1a4fc8a6c3df07d21468cd4bdaf552f061a2bc42
Risa Fujii <risa@kray.jp>
2020-02-08 14:25:31 +0900

    Scan Dynamo table (doesn't work until role added)

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const axios = require('axios')
 const AWS = require('aws-sdk')
+AWS.config.update({region: 'ap-northeast-1'});
 const dynamo = new AWS.DynamoDB.DocumentClient()
 
 exports.handler = async (event) => {
   const nytList = await getNytList().catch((error) => {
-    console.log(`Error: ${error}`)
+    console.log(`NYT error: ${error}`)
   })
 
   if (nytList) {
     console.log(nytList)
   }
+
+  const scanParams = {
+    TableName: 'weekly-bestseller-updates'
+  }
+  const scanResult = await dynamo.scan(scanParams).promise().catch((error) => {
+    console.log(`Scan error: ${error}`)
+  })
+
+  console.log(scanResult)
 }
 
 async function getNytList() {
