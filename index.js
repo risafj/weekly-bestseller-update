c825c4a3d98fc3bfc885e4e21300279dd856ec93
Risa Fujii <risaf1231@gmail.com>
2020-02-24 16:07:27 +0900

    Append new books in Dynamo table

--- index.js
+++ index.js
@@ -12,22 +12,24 @@ exports.handler = async (event) => {
   const nytList = await getNytList().catch((error) => {
     console.log(`NYT error: ${error}`)
   })
+  const nytIsbns = nytList.data.results.books.map(book => book.primary_isbn13)
 
-  if (nytList) {
-    console.log(nytList)
-  }
-
-  const scanResult = await scanDynamoTable().catch((error) => {
+  const userTable = await scanDynamoTable().catch((error) => {
     console.log(`Scan error: ${error}`)
   })
 
-  console.log(scanResult)
+  // Using a for loop because forEach does not work well with async.
+  for (let i = 0; i < userTable.Items.length; i++) {
+    await appendNewBooksInDynamo(userTable.Items[i], nytIsbns).catch((error) => {
+      console.log(`Update error: ${error}`)
+    })
+  }
+  console.log('Done')
 }
 
 async function getNytList () {
   const nytBookRootUrl = 'https://api.nytimes.com/svc/books/v3/lists/current'
   const fictionPath = '/combined-print-and-e-book-fiction.json'
-  // const nonFictionPath = '/combined-print-and-e-book-nonfiction.json'
   const params = { 'api-key': process.env.NYT_API_KEY }
 
   return axios.get(
@@ -38,7 +40,33 @@ async function getNytList () {
 
 async function scanDynamoTable () {
   const scanParams = {
-    TableName: 'weekly-bestseller-updates'
+    TableName: process.env.DYNAMO_TABLE_NAME
   }
   return dynamo.scan(scanParams).promise()
+}
+
+async function appendNewBooksInDynamo (user, nytIsbns) {
+  let newBooks = null
+  if (user.books && user.books.values.length !== 0) {
+    newBooks = nytIsbns.filter(i => !user.books.values.includes(i))
+  } else {
+    newBooks = nytIsbns
+  }
+
+  if (newBooks.length === 0) {
+    console.log(`Skipping because there are no new books to send for user ${user.email}`)
+    return
+  }
+  const updateParams = {
+    TableName: process.env.DYNAMO_TABLE_NAME,
+    Key: {
+      email: user.email
+    },
+    UpdateExpression: 'ADD books :new_books',
+    ExpressionAttributeValues: {
+      ':new_books': dynamo.createSet(newBooks)
+    }
+  }
+  console.log(`About to update user ${user.email}`)
+  return dynamo.update(updateParams).promise()
 }
